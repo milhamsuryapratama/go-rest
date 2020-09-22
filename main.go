747198035e5d201b15e0b897c706d27dae0c2bb9
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-09-22 12:58:14 +0700

    create route delete kelas

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	http.HandleFunc("/kelas", kelas.GetAll)
 	http.HandleFunc("/kelas/create", kelas.CreateKelas)
+	http.HandleFunc("/kelas/delete", kelas.DeleteKelas)
 	fmt.Println("Server running")
 	http.ListenAndServe(":9000", nil)
 }
